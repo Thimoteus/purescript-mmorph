0986b210485712194d03f4deb37a2633118b7446
Phil Freeman <paf31@cantab.net>
2016-08-09 20:43:16 -0700

    Updates for 0.9.3

--- docs/Control/Monad/Morph.md
+++ docs/Control/Monad/Morph.md
@@ -6,78 +6,88 @@ A port of Haskell's [mmorph library](http://hackage.haskell.org/package/mmorph-1
 
 ``` purescript
 class MFunctor t where
-  hoist :: forall m n a b. (Monad m) => (forall a. m a -> n a) -> t m b -> t n b
+  hoist :: forall m n. Monad m => m ~> n -> (t m) ~> (t n)
 ```
 
 ##### Instances
 ``` purescript
-instance mfunctorExceptT :: MFunctor (ExceptT e)
-instance mfunctorMaybe :: MFunctor MaybeT
-instance mfunctorReaderT :: MFunctor (ReaderT r)
-instance mfunctorWriterT :: MFunctor (WriterT w)
-instance mfunctorStateT :: MFunctor (StateT s)
-instance mfunctorRWS :: MFunctor (RWST r w s)
-instance mfunctorCompose :: (Functor f) => MFunctor (Compose f)
-instance mfunctorProduct :: MFunctor (Product f)
+MFunctor (ExceptT e)
+MFunctor MaybeT
+MFunctor (ReaderT r)
+MFunctor (WriterT w)
+MFunctor (StateT s)
+MFunctor (RWST r w s)
+(Functor f) => MFunctor (Compose f)
+MFunctor (Product f)
 ```
 
 #### `generalize`
 
 ``` purescript
-generalize :: forall m a. (Monad m) => Identity a -> m a
+generalize :: forall m a. Monad m => Identity a -> m a
 ```
 
 #### `MMonad`
 
 ``` purescript
 class (MFunctor t, MonadTrans t) <= MMonad t where
-  embed :: forall n m b. (Monad n) => (forall a. m a -> t n a) -> t m b -> t n b
+  embed :: forall n m b. Monad n => (forall a. m a -> t n a) -> t m b -> t n b
 ```
 
 ##### Instances
 ``` purescript
-instance mmonadExceptT :: MMonad (ExceptT e)
-instance mmonadMaybeT :: MMonad MaybeT
-instance mmonadReaderT :: MMonad (ReaderT r)
-instance mmonadWriterT :: (Monoid w) => MMonad (WriterT w)
+MMonad (ExceptT e)
+MMonad MaybeT
+MMonad (ReaderT r)
+(Monoid w) => MMonad (WriterT w)
 ```
 
 #### `squash`
 
 ``` purescript
-squash :: forall a m t. (Monad m, MMonad t) => t (t m) a -> t m a
+squash :: forall m t. (Monad m, MMonad t) => (t (t m)) ~> (t m)
 ```
 
 #### `(>|>)`
 
 ``` purescript
-(>|>) :: forall m1 m2 m3 t c. (MMonad t, Monad m3) => (forall a. m1 a -> t m2 a) -> (forall b. m2 b -> t m3 b) -> m1 c -> t m3 c
+infixr 2 composeKleisliRight as >|>
 ```
-
-_right-associative / precedence 2_
 
 #### `(<|<)`
 
 ``` purescript
-(<|<) :: forall m1 m2 m3 t c. (MMonad t, Monad m3) => (forall b. m2 b -> t m3 b) -> (forall a. m1 a -> t m2 a) -> m1 c -> t m3 c
+infixl 2 composeKleisliLeft as <|<
 ```
-
-_left-associative / precedence 2_
 
 #### `(=<|)`
 
 ``` purescript
-(=<|) :: forall t m n b. (MMonad t, Monad n) => (forall a. m a -> t n a) -> t m b -> t n b
+infixr 2 embed as =<|
 ```
-
-_right-associative / precedence 2_
 
 #### `(|>=)`
 
 ``` purescript
-(|>=) :: forall t m n b. (MMonad t, Monad n) => t m b -> (forall a. m a -> t n a) -> t n b
+infixl 2 flipEmbed as |>=
 ```
 
-_left-associative / precedence 2_
+#### `composeKleisliRight`
+
+``` purescript
+composeKleisliRight :: forall m1 m2 m3 t. (MMonad t, Monad m3) => m1 ~> (t m2) -> m2 ~> (t m3) -> m1 ~> (t m3)
+```
+
+#### `composeKleisliLeft`
+
+``` purescript
+composeKleisliLeft :: forall m1 m2 m3 t. (MMonad t, Monad m3) => m2 ~> (t m3) -> m1 ~> (t m2) -> m1 ~> (t m3)
+```
+
+#### `flipEmbed`
+
+``` purescript
+flipEmbed :: forall t m n a. (MMonad t, Monad n) => t m a -> m ~> (t n) -> t n a
+```
 
 
--- src/Control/Monad/Morph.purs
+++ src/Control/Monad/Morph.purs
@@ -3,31 +3,27 @@ module Control.Monad.Morph where
 
 import Prelude
 
-import Control.Monad.Trans (MonadTrans, lift)
-import qualified Control.Monad.Except.Trans as E
-import qualified Control.Monad.Maybe.Trans as M
-import qualified Control.Monad.Reader.Trans as R
-import qualified Control.Monad.RWS.Trans as RWS
-import qualified Control.Monad.State.Trans as S
-import qualified Control.Monad.Writer.Trans as W
-import qualified Control.Monad.List.Trans as L
+import Control.Monad.Except.Trans as E
 
-import Data.Monoid (Monoid)
-import Data.Either (Either(Left, Right))
-import Data.Maybe (Maybe(Just, Nothing))
-import Data.Functor.Compose (Compose(Compose))
-import Data.Identity (runIdentity, Identity(Identity))
-import Data.Functor.Product (Product(Product))
-import Data.Tuple (Tuple(Tuple))
+import Control.Monad.Maybe.Trans as M
+import Control.Monad.Reader.Trans as R
+import Control.Monad.RWS.Trans as RWS
+import Control.Monad.State.Trans as S
+import Control.Monad.Trans (class MonadTrans)
+import Control.Monad.Writer.Trans as W
+import Data.Either (Either(..))
+import Data.Functor.Compose (Compose(..))
+import Data.Functor.Product (Product(..))
+import Data.Identity (runIdentity, Identity)
+import Data.Maybe (Maybe(..))
+import Data.Monoid (class Monoid)
+import Data.Tuple (Tuple(..))
 
 class MFunctor t where
-  hoist :: forall m n a b. (Monad m) => (forall a. m a -> n a) -> t m b -> t n b
+  hoist :: forall m n. Monad m => m ~> n -> t m ~> t n
 
 instance mfunctorExceptT :: MFunctor (E.ExceptT e) where
   hoist nat m = E.ExceptT (nat (E.runExceptT m))
-
---instance mfunctorList :: MFunctor L.ListT where
-  --hoist nat m = L.ListT (nat (L.runListT m))
 
 instance mfunctorMaybe :: MFunctor M.MaybeT where
   hoist nat m = M.MaybeT (nat (M.runMaybeT m))
@@ -50,64 +46,65 @@ instance mfunctorCompose :: (Functor f) => MFunctor (Compose f) where
 instance mfunctorProduct :: MFunctor (Product f) where
   hoist nat (Product (Tuple f g)) = Product (Tuple f (nat g))
 
-generalize :: forall m a. (Monad m) => Identity a -> m a
+generalize :: forall m a. Monad m => Identity a -> m a
 generalize = pure <<< runIdentity
 
 class (MFunctor t, MonadTrans t) <= MMonad t where
-  embed :: forall n m b. (Monad n) => (forall a. m a -> t n a) -> t m b -> t n b
+  embed :: forall n m b. Monad n => (forall a. m a -> t n a) -> t m b -> t n b
 
-squash :: forall a m t. (Monad m, MMonad t) => t (t m) a -> t m a
+squash :: forall m t. (Monad m, MMonad t) => t (t m) ~> t m
 squash = embed id
 
-infixr 2 >|>
-infixr 2 =<|
-infixl 2 <|<
-infixl 2 |>=
+infixr 2 composeKleisliRight as >|>
+infixl 2 composeKleisliLeft as <|<
 
-(>|>) :: forall m1 m2 m3 t c. (MMonad t, Monad m3) => (forall a. m1 a -> t m2 a)
-                                                   -> (forall b. m2 b -> t m3 b)
-                                                   ->            m1 c -> t m3 c
-(>|>) f g m = embed g (f m)
+infixr 2 embed as =<|
+infixl 2 flipEmbed as |>=
 
-(<|<) :: forall m1 m2 m3 t c. (MMonad t, Monad m3) => (forall b. m2 b -> t m3 b)
-                                                   -> (forall a. m1 a -> t m2 a)
-                                                   ->            m1 c -> t m3 c
-(<|<) g f m = embed g (f m)
+composeKleisliRight
+  :: forall m1 m2 m3 t
+   . (MMonad t, Monad m3)
+  => m1 ~> t m2
+  -> m2 ~> t m3
+  -> m1 ~> t m3
+composeKleisliRight f g m = embed g (f m)
 
-(=<|) :: forall t m n b. (MMonad t, Monad n) => (forall a. m a -> t n a)
-                                             ->          t m b -> t n b
-(=<|) = embed
+composeKleisliLeft
+  :: forall m1 m2 m3 t
+   . (MMonad t, Monad m3)
+  => m2 ~> t m3
+  -> m1 ~> t m2
+  -> m1 ~> t m3
+composeKleisliLeft g f m = embed g (f m)
 
-(|>=) :: forall t m n b. (MMonad t, Monad n) => t m b
-                                             -> (forall a. m a -> t n a)
-                                             -> t n b
-(|>=) t f = embed f t
+flipEmbed
+  :: forall t m n a
+   . (MMonad t, Monad n)
+  => t m a
+  -> m ~> t n
+  -> t n a
+flipEmbed t f = embed f t
 
 instance mmonadExceptT :: MMonad (E.ExceptT e) where
-  embed f m = E.ExceptT (do
-                        x <- E.runExceptT (f (E.runExceptT m))
-                        return (case x of
-                               Left e -> Left e
-                               Right (Left e) -> Left e
-                               Right (Right a) -> Right a))
-
---instance mmonadListT :: MMonad L.ListT where
-  --embed f m = L.ListT (do
-                      --x <- L.runListT (f (L.runListT m))
-                      --return (concat x))
+  embed f m = E.ExceptT do
+    x <- E.runExceptT (f (E.runExceptT m))
+    pure case x of
+      Left e -> Left e
+      Right (Left e) -> Left e
+      Right (Right a) -> Right a
 
 instance mmonadMaybeT :: MMonad M.MaybeT where
-  embed f m = M.MaybeT (do
-                       x <- M.runMaybeT (f (M.runMaybeT m))
-                       return (case x of
-                              Nothing -> Nothing
-                              Just Nothing -> Nothing
-                              Just (Just a) -> Just a))
+  embed f m = M.MaybeT do
+    x <- M.runMaybeT (f (M.runMaybeT m))
+    pure case x of
+      Nothing -> Nothing
+      Just Nothing -> Nothing
+      Just (Just a) -> Just a
 
 instance mmonadReaderT :: MMonad (R.ReaderT r) where
-  embed f m = R.ReaderT (\ i -> R.runReaderT (f (R.runReaderT m i)) i)
+  embed f m = R.ReaderT \i -> R.runReaderT (f (R.runReaderT m i)) i
 
 instance mmonadWriterT :: (Monoid w) => MMonad (W.WriterT w) where
-  embed f m = W.WriterT (do
-                        Tuple (Tuple a w1) w2 <- W.runWriterT (f (W.runWriterT m))
-                        return (Tuple a (append w1 w2)))
+  embed f m = W.WriterT do
+    Tuple (Tuple a w1) w2 <- W.runWriterT (f (W.runWriterT m))
+    pure (Tuple a (append w1 w2))
